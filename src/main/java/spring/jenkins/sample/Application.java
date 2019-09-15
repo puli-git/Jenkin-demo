36c0d435a5d5af534fe8d123c5f935a77297cce1
unknown <pulithevan.sp@gmail.com>
2019-09-15 18:46:23 +0530

    Commit to new repository

---
+++ src/main/java/spring/jenkins/sample/Application.java
@@ -0,0 +1,33 @@
+package spring.jenkins.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+//Added new line
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+		}
+	
+	@RequestMapping("/")
+	public String status(){
+		return "Spring Jenkins is UP";
+	}
+	
+	public void test() {
+		
+		String name="Saurabh Delh Dei";
+		System.out.println("Added fcode for jenkins demo");
+		}
+	public void display(){
+		System.out.println("Added Method to test Jenkin");
+		System.out.println("Added new line...");
+		
+	}
+	
+}
